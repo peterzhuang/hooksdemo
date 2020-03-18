52a1c27167d809670706f7cb2cab4c804175bf69
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2020-03-18 17:54:21 -0400

    useImperativeHandle demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Profile from './components/Profile';
+import Usage from './components/Usage';
 
 function App() {
   return (
-    <Profile />
+    // <Profile />
+    <Usage />
   );
 }
 
